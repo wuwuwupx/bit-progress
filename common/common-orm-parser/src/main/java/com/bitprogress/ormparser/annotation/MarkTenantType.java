ab03cdc6234225085d9ea05d8f3dbbd3960e67d2
px.wu <896590620@qq.com>
2025-03-28 09:27:49 +0800

    去除多余注解

--- common/common-orm-parser/src/main/java/com/bitprogress/ormparser/annotation/MarkTenantType.java
+++ common/common-orm-parser/src/main/java/com/bitprogress/ormparser/annotation/MarkTenantType.java
@@ -6,10 +6,6 @@ import java.lang.annotation.*;
 
 /**
  * 标记当前方法需要指定的租户类型
- * 会被以下三个注解覆盖
- * @see CurrentTenant
- * @see OperateTenant
- * @see AllTenant
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
--- common/common-orm-parser/src/main/java/com/bitprogress/ormparser/aspect/TenantTypeAspect.java
+++ common/common-orm-parser/src/main/java/com/bitprogress/ormparser/aspect/TenantTypeAspect.java
@@ -1,11 +1,8 @@
 package com.bitprogress.ormparser.aspect;
 
-import com.bitprogress.ormparser.annotation.AllTenant;
-import com.bitprogress.ormparser.annotation.CurrentTenant;
-import com.bitprogress.ormparser.annotation.MarkTenantType;
-import com.bitprogress.ormparser.annotation.OperateTenant;
-import com.bitprogress.ormmodel.enums.TenantType;
 import com.bitprogress.ormcontext.utils.TenantContextUtils;
+import com.bitprogress.ormmodel.enums.TenantType;
+import com.bitprogress.ormparser.annotation.MarkTenantType;
 import lombok.SneakyThrows;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -18,18 +15,10 @@ import java.util.Objects;
 @Component
 public class TenantTypeAspect {
 
-    @Around(value = "@annotation(currentTenant) || @annotation(operateTenant) || @annotation(allTenant) || @annotation(markTenantType)", argNames = "point,currentTenant,operateTenant,allTenant,markTenantType")
+    @Around(value = "@annotation(markTenantType)")
     @SneakyThrows
-    public Object around(ProceedingJoinPoint point,
-                         CurrentTenant currentTenant,
-                         OperateTenant operateTenant,
-                         AllTenant allTenant,
-                         MarkTenantType markTenantType) {
-        TenantType tenantType = Objects.nonNull(currentTenant)
-                ? TenantType.CURRENT
-                : Objects.nonNull(operateTenant) ? TenantType.OPERATE
-                : Objects.nonNull(allTenant) ? TenantType.ALL
-                : Objects.nonNull(markTenantType) ? markTenantType.tenantType() : TenantType.CURRENT;
+    public Object around(ProceedingJoinPoint point, MarkTenantType markTenantType) {
+        TenantType tenantType = Objects.nonNull(markTenantType) ? markTenantType.tenantType() : TenantType.CURRENT;
         TenantType existTenantType = TenantContextUtils.getTenantType();
         try {
             TenantContextUtils.setTenantTypeOrThrow(tenantType, "未初始化租户信息，非法操作");
