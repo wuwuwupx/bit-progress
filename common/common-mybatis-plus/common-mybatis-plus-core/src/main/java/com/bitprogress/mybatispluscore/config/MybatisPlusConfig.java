e62242d1018760b4fed6fb7c428227abdce4f50d
wpx <896590620@qq.com>
2025-03-31 20:29:01 +0800

    调整架构

---
+++ common/common-mybatis-plus/common-mybatis-plus-core/src/main/java/com/bitprogress/mybatispluscore/config/MybatisPlusConfig.java
@@ -0,0 +1,61 @@
+package com.bitprogress.mybatispluscore.config;
+
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.bitprogress.mybatispluscore.handler.DataScopeLineHandler;
+import com.bitprogress.mybatispluscore.handler.DefaultDataScopeLineHandler;
+import com.bitprogress.mybatispluscore.handler.TenantIdLineHandler;
+import com.bitprogress.mybatispluscore.plugins.TenantSqlInnerInterceptor;
+import com.bitprogress.mybatispluscore.properties.DataScopeProperties;
+import com.bitprogress.mybatispluscore.properties.TenantProperties;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@AutoConfigureAfter(MybatisPlusAutoConfiguration.class)
+@MapperScan("com.bitprogress.**.mapper")
+@Configuration
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableConfigurationProperties({TenantProperties.class, DataScopeProperties.class})
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor(TenantProperties tenantProperties,
+                                                        DataScopeProperties dataScopeProperties) {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        // 租户插件
+        TenantIdLineHandler tenantSqlHandler = new TenantIdLineHandler(tenantProperties);
+        DataScopeLineHandler dataScopeLineHandler = new DefaultDataScopeLineHandler(dataScopeProperties);
+        TenantSqlInnerInterceptor sqlInnerInterceptor = new TenantSqlInnerInterceptor(tenantSqlHandler, dataScopeLineHandler);
+        interceptor.addInnerInterceptor(sqlInnerInterceptor);
+
+        // 乐观锁插件
+        OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor = new OptimisticLockerInnerInterceptor();
+        interceptor.addInnerInterceptor(optimisticLockerInnerInterceptor);
+
+        // 分页插件
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
+
+    /**
+     * 逻辑删除插件
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new DefaultSqlInjector();
+    }
+
+}
---
+++ common/common-mybatis-plus/common-mybatis-plus-core/src/main/java/com/bitprogress/mybatispluscore/properties/DataScopeProperties.java
@@ -0,0 +1,50 @@
+package com.bitprogress.mybatispluscore.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * mybatis plus 数据权限插件配置
+ */
+@Data
+@ConfigurationProperties("mybatis-plus.data-scope")
+public class DataScopeProperties {
+
+    /**
+     * 启用数据权限插件
+     * 默认不启用
+     */
+    private Boolean enabled = false;
+
+    /**
+     * 白名单
+     */
+    private List<String> ignoreTables = new ArrayList<>();
+
+    /**
+     * 启用名单
+     * 为空表示所有的表都启用，不为空则启动配置的表
+     * 当启用名单不为空时，优先级高于 白名单
+     */
+    private List<String> enableTables = new ArrayList<>();
+
+    /**
+     * 数据权限字段
+     * key 表名
+     * value 数据权限字段
+     */
+    private Map<String, String> dataScopeColumn = new HashMap<>();
+
+    /**
+     * 匹配当前用户数据权限的字段
+     * key 表名
+     * value 数据权限字段
+     */
+    private Map<String, String> selfWhereColumn = new HashMap<>();
+
+}
