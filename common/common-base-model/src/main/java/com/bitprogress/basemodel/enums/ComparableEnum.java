bcac2b781aaacbaae037c5d138b54b2801424914
wpx <896590620@qq.com>
2025-04-02 23:50:02 +0800

    feat(base-model): adjust enums 新增集合类型

---
+++ common/common-base-model/src/main/java/com/bitprogress/basemodel/enums/ComparableEnum.java
@@ -0,0 +1,9 @@
+package com.bitprogress.basemodel.enums;
+
+import com.bitprogress.basemodel.IEnum;
+
+/**
+ * about comparable enum
+ */
+public interface ComparableEnum extends IEnum {
+}
---
+++ common/common-base-model/src/main/java/com/bitprogress/basemodel/enums/TimeEnum.java
@@ -0,0 +1,9 @@
+package com.bitprogress.basemodel.enums;
+
+import com.bitprogress.basemodel.IEnum;
+
+/**
+ * about time's enum
+ */
+public interface TimeEnum extends IEnum {
+}
