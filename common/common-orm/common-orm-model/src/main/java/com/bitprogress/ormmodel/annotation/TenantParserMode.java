8827612400776b777f7f857113bd31450e5182d1
wpx <896590620@qq.com>
2025-04-13 02:41:26 +0800

    feat(common-orm): feat TenantParserMode.java
    编辑注释

--- common/common-orm/common-orm-model/src/main/java/com/bitprogress/ormmodel/annotation/TenantParserMode.java
+++ common/common-orm/common-orm-model/src/main/java/com/bitprogress/ormmodel/annotation/TenantParserMode.java
@@ -7,7 +7,6 @@ import java.lang.annotation.*;
 /**
  * sql解析模式
  * 本注解主要是用于规避 sql解析，所以默认的解析模式是忽略解析
- * 解析模式的开启表现为 {@link SqlParserContext#getSqlParserMsg()} != null && {@link SqlParserMsg#getEnable()} == true
  * 当前方法开启新的解析模式的时候，会将上层方法的解析模式状态擦除，直到当前方法结束。
  * 假设 （1）方法A的解析方式为过滤解析，sql类型为 NONE  （2）方法B解析方式为强制解析，sql类型为 SELECT
  * 当方法A调用方法B的时，方法B中的 UPDATE 语句仍会被插件进行默认解析处理（实际是否进行处理，最终以系统是否开启sql处理插件为准）
@@ -22,9 +21,6 @@ import java.lang.annotation.*;
  * 引入 sql类型
  * - 包含 {@link SqlType#NONE} 则表示对所有 sql类型生效
  * <p>
- * 引入租户模式开关
- * - 默认不开启
- * <p>
  * 引入租户类型，默认当前租户
  * - {@link TenantType#ALL} 所有租户，sql解析后设置租户ID为操作用户可操作的租户ID
  * - {@link TenantType#CURRENT} 当前租户，sql解析后设置租户ID为操作用户当前的租户ID
@@ -34,18 +30,6 @@ import java.lang.annotation.*;
  * 引入rpc传播方式
  * - true，rpc调用传播当前解析模式
  * - false，rpc调用不传播当前解析模式
- * <p>
- * 引入数据范围模式开关
- * 默认不开启
- * <p>
- * 引入数据范围类型
- * 默认为自身
- * - {@link DataScopeType#ALL} 全部，不需要做数据范围覆盖
- * - {@link DataScopeType#COMPOSITE_LEVEL} 数据范围，sql解析后设置数据范围为操作用户当前的数据范围
- * - {@link DataScopeType#SELF} 自身，sql解析后设置数据范围为操作用户当前的数据范围
- * <p>
- * 可考虑将当前注解拆开为全局解析开关、租户、数据权限注解（参考mybatis-plus原有的租户和数据权限模式）
- * 但是考虑到传播属性和优先级设计等问题，且当前注解的本意是为了一键去除租户模式和数据权限模式而创建，所以暂不进行拆分
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
