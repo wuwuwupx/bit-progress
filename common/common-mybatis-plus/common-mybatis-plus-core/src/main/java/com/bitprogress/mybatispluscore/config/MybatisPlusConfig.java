c4ae412ee9d6bd9c3d2a9bfaf77e27aa783dd455
wpx <896590620@qq.com>
2025-04-14 21:08:33 +0800

    feat(common-mybatis-plus): feat config
    完善插件引入

--- common/common-mybatis-plus/common-mybatis-plus-core/src/main/java/com/bitprogress/mybatispluscore/config/MybatisPlusConfig.java
+++ common/common-mybatis-plus/common-mybatis-plus-core/src/main/java/com/bitprogress/mybatispluscore/config/MybatisPlusConfig.java
@@ -13,7 +13,6 @@ import com.bitprogress.mybatispluscore.handler.TenantHandler;
 import com.bitprogress.mybatispluscore.interceptor.TenantSqlInnerInterceptor;
 import com.bitprogress.mybatispluscore.properties.DataScopeProperties;
 import com.bitprogress.mybatispluscore.properties.TenantProperties;
-import com.bitprogress.ormcontext.service.DataScopeContextService;
 import com.bitprogress.ormcontext.service.TenantContextService;
 import com.bitprogress.ormcontext.service.impl.SingleTypeDataScopeContextService;
 import com.bitprogress.ormcontext.service.impl.TenantContextServiceImpl;
@@ -23,6 +22,8 @@ import com.bitprogress.ormparser.Service.impl.SingleTypeDataScopeOrmDataService;
 import com.bitprogress.ormparser.Service.impl.TenantOrmDataServiceImpl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -57,32 +58,39 @@ public class MybatisPlusConfig {
     }
 
     @Bean
+    @ConditionalOnMissingBean(TenantHandler.class)
     public TenantHandler tenantSqlHandler(TenantProperties tenantProperties, TenantOrmDataService ormDataService) {
         return new DefaultTenantHandler(tenantProperties, ormDataService);
     }
 
     @Bean
+    @ConditionalOnMissingBean(TenantOrmDataService.class)
     public TenantOrmDataService tenantOrmDataService(TenantContextService tenantContextService) {
         return new TenantOrmDataServiceImpl(tenantContextService);
     }
 
     @Bean
+    @ConditionalOnMissingBean(TenantContextService.class)
     public TenantContextService tenantContextService() {
         return new TenantContextServiceImpl();
     }
 
     @Bean
+    @ConditionalOnMissingBean(DataScopeHandler.class)
+    @ConditionalOnProperty(name = "mybatis-plus.data-scope.mode", havingValue = "SINGLE", matchIfMissing = true)
     public DataScopeHandler dataScopeHandler(DataScopeProperties dataScopeProperties,
                                              DataScopeOrmDataService ormDataService) {
         return new SingleTypeDataScopeHandler(dataScopeProperties, ormDataService);
     }
 
     @Bean
+    @ConditionalOnProperty(name = "mybatis-plus.data-scope.mode", havingValue = "SINGLE", matchIfMissing = true)
     public DataScopeOrmDataService dataScopeOrmDataService(SingleTypeDataScopeContextService dataScopeContextService) {
         return new SingleTypeDataScopeOrmDataService(dataScopeContextService);
     }
 
     @Bean
+    @ConditionalOnProperty(name = "mybatis-plus.data-scope.mode", havingValue = "SINGLE", matchIfMissing = true)
     public SingleTypeDataScopeContextService dataScopeContextService() {
         return new SingleTypeDataScopeContextService();
     }
