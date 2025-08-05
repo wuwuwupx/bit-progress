c8ddc3dad72c56c36458a487b64dc5a39c5d8b4f
wpx <896590620@qq.com>
2025-08-05 19:47:44 +0800

    refactor(project): 重构项目模块并更新依赖
    - 将 common-server 重命名为 common-application
    - 更新了多个模块的依赖版本- 调整了部分模块的结构和内容

---
+++ common/common-application/common-application-basic/src/main/java/com/bitprogress/applicationbasic/config/ServerConfig.java
@@ -0,0 +1,10 @@
+package com.bitprogress.applicationbasic.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@EnableAutoConfiguration
+@Configuration
+public class ServerConfig {
+
+}
